6d9dbaf509fc68150011ab430cdf19cd5f53c0e1
Tijo <hamsen3880@outlook.com>
2024-10-23 02:13:01 +0530

    Footer and main part

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -17,8 +17,29 @@ let steps = [
         name: "Kyalami",
         icon: "fa-solid fa-music",
         description: "LoL",
+    }
+]
+
+
+let benefits = [
+    {
+        metric: "10x",
+        name: "Website Development",
+        description: "Making full stack websites using React, NextJS, ExpressJS, Django, FastAPI. Got good command in Javascript and Python.",
     },
-];
+    {
+        name: "Application Development",
+        description: "Making cross-platform mobile application (specifically for Android and iOS using Flutter.",
+    },
+    {
+        name: "Dev-Ops",
+        description: "Doing CI/CD using Gitlab and contenerization using Docker. Knowledge of cloud platforms like AWS.",
+    },
+    {
+        name: "Design and UI/UX",
+        description: "Making designs, templates and architecture workflow diagrams and demonstrations using Figma and Canva.",
+    }
+]
 
 </script>
 
@@ -65,6 +86,31 @@ let steps = [
             <Step step={steps[2]}>
                 <p> Kyalami is a music web application made using <strong class="text-violet-400"> Django</strong>. Users can listen to music here. </p>
             </Step>
+        </div>
+    </section>
+    <section id="about" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+        <div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4">
+            <h6 class="text-lg sm:text-xl md:text-2xl"> Want to know me more? </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="text-violet-400"> about </span> me.
+            </h3>
+        </div>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
         </div>
     </section>
 </main>
